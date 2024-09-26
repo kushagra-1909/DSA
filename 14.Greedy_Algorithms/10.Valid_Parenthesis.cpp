7111071e3f210ef2f5416fb661e9802cf20f1c57
kushagra-1909 <kushagraface123@gmail.com>
2024-09-26 10:29:32 +0530

    adding greedy problems

--- 14.Greedy_Algorithms/10.Valid_Parenthesis.cpp
+++ 14.Greedy_Algorithms/10.Valid_Parenthesis.cpp
@@ -16,3 +16,28 @@ int f(s,ind,cnt){
 
     return (f(s,ind+1,cnt+1) | f(s,ind+1,cnt-1) | f(s,ind+1,cnt));
 }
+
+
+// optimal approach
+fun(s){
+    min = 0, max=0;
+    for(int i=0; i<N; i++){
+        if(s[i] == '('){
+            min++;
+            max++;
+        }
+        else if(s[i] == ')'){
+            min = min-1;
+            max = max-1;   
+        }
+        else{
+            min = min-1;
+            max = max+1
+        }
+
+        if(min < 0) min = 0;
+        if(max < 0) return false;
+    }
+
+    return (min==0);
+}
